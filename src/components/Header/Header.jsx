e479387acebb302620ce1aeb84b0b0091ac3e32c
Anastasiia Vorobets <anastasiiavorobets03@gmail.com>
2023-10-07 19:04:26 +0300

    edited header logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,11 @@ import logo from '../../images/logo.png'
 function Header() {
   return (
     <header className="header">
-      <div className='header__name'>
-        <img src={logo} alt="vnv_solutions" className='header__logo'/>
-        <h1 className='header__logo-name'>SOLUTIONS</h1>
-      </div>
+      <img
+        src={logo}
+        alt="vnv_solutions"
+        className='header__logo'
+      />
       <p className='header__description'>Easy, quality, IT - winning solutions for your business</p>
       <div className='header__inner'>
         <h2 className='header__title'>/ IT company </h2>
